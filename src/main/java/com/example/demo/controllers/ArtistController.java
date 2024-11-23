43d39140be76b13f9112c0a4d75de7117ff01a0c
Ömer Feyzioğlu <oomerfeyzioglu@gmail.com>
2024-11-23 23:29:13 +0300

    second method delete

--- src/main/java/com/example/demo/controllers/ArtistController.java
+++ src/main/java/com/example/demo/controllers/ArtistController.java
@@ -42,13 +42,7 @@ public class ArtistController {
 
     }
 
-    @PostMapping("/x/y/z")
-    public ResponseEntity<String> tillString(){
 
-
-        return ResponseEntity.ok("api updated");
-
-    }
 
 
     @PostMapping("/x/y/z/f")
